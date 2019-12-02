ad98e4b5d4a815259a04265681b793fa1fbf7c04
azimgivron <azimgivron@gmail.com>
2019-12-02 23:09:29 +0100

    add select signal

--- Code/RiscvMiniBase/src/main/scala/ALU_Dispatcher.scala
+++ Code/RiscvMiniBase/src/main/scala/ALU_Dispatcher.scala
@@ -9,21 +9,23 @@ import freechips.rocketchip.config.Parameters
 class DispatcherIo(implicit p: Parameters) extends CoreBundle()(p) {
   val A 			= Input(UInt(xlen.W)) //first operand
   val B 			= Input(UInt(xlen.W)) //second operand
-  val alu_op 		= Input(UInt(4.W)) //operation
+  val alu_op 		= Input(UInt(4.W)) //operator
   val from_alu1		= Input(Bool()) //status updater signal for alu1
   val from_alu2		= Input(Bool())	//status updater signal for alu2
   val A_toALU1 		= Output(UInt(xlen.W)) //first operand
   val B_toALU1 		= Output(UInt(xlen.W)) //second operand
-  val alu_op_toALU1 = Output(UInt(4.W)) //operation
+  val alu_op_toALU1 = Output(UInt(4.W)) //operator
   val A_toALU2 		= Output(UInt(xlen.W)) //first operand
   val B_toALU2 		= Output(UInt(xlen.W)) //second operand
-  val alu_op_toALU2 = Output(UInt(4.W)) //operation
+  val alu_op_toALU2 = Output(UInt(4.W)) //operator
+  val select 		= Output(Bool()) //Tells which one of the ALUs contains the oldest result
 }
 
 class ALU_Dispatcher(implicit val p: Parameters) extends Module with CoreParams {
 	val io			= IO(new DispatcherIo)
 	val alu1_busy 	= RegInit(false.B)
 	val alu2_busy 	= RegInit(false.B)
+	val select		= RegInit(false.B)
 
 	//FSM states
 	val dispatchToALU1 :: dispatchToALU2 :: waitStatus :: Nil = Enum(3)
@@ -36,31 +38,42 @@ class ALU_Dispatcher(implicit val p: Parameters) extends Module with CoreParams 
   	//You can only reach dispatchToALU1 if alu1_busy was already set to false
   		alu1_busy := false.B
   	}
+
   	when(io.from_alu2 && state =/= dispatchToALU2) {
   	//Similarly you can only reach dispatchToALU2 if alu2_busy was already set to false
   		alu2_busy := false.B
   	}
 
+  	when((io.from_alu2 && state =/= dispatchToALU2) && (alu1_busy || state === dispatchToALU1)) {
+		select 		:= false.B
+		io.select 	:= false.B
+  	}.elsewhen((io.from_alu1 && state =/= dispatchToALU1) && (alu2_busy || state === dispatchToALU2)) {
+		select 		:= true.B
+		io.select 	:= true.B
+  	}.otherwise {
+  		io.select := select
+  	}
+
 	//FSM
 	//match the current state and act in consequence
 	when(state === dispatchToALU1) { //used when instead of switch because switch doesn't allow default case
-    	when(!alu1_busy){
+    	when(!alu1_busy) {
     		//assign outputs
-	    	io.A_toALU1 := io.A
-	    	io.B_toALU1 := io.B
-	    	io.alu_op_toALU1 := io.alu_op
-	    	io.A_toALU2 := 0.U
-	    	io.B_toALU2 := 0.U
-	    	io.alu_op_toALU2 := 0.U
+	    	io.A_toALU1 		:= io.A
+	    	io.B_toALU1			:= io.B
+	    	io.alu_op_toALU1	:= io.alu_op
+	    	io.A_toALU2 		:= 0.U
+	    	io.B_toALU2 		:= 0.U
+	    	io.alu_op_toALU2 	:= 0.U
 	    	//change internal status to busy
-		  	alu1_busy := true.B
-    	}.otherwise{
-    		io.A_toALU1 := 0.U
-	    	io.B_toALU1 := 0.U
-	    	io.alu_op_toALU1 := 0.U
-			io.A_toALU2 := 0.U
-	    	io.B_toALU2 := 0.U
-	    	io.alu_op_toALU2 := 0.U
+		  	alu1_busy 			:= true.B
+    	}.otherwise {
+    		io.A_toALU1 		:= 0.U
+	    	io.B_toALU1 		:= 0.U
+	    	io.alu_op_toALU1 	:= 0.U
+			io.A_toALU2 		:= 0.U
+	    	io.B_toALU2 		:= 0.U
+	    	io.alu_op_toALU2 	:= 0.U
     	}
     	//transition of state    		
 		when(!alu2_busy || io.from_alu2) {
@@ -70,36 +83,36 @@ class ALU_Dispatcher(implicit val p: Parameters) extends Module with CoreParams 
 		}
     }
     .elsewhen(state === dispatchToALU2) {
-		when(!alu2_busy){
-			io.A_toALU1 := 0.U
-	    	io.B_toALU1 := 0.U
-	    	io.alu_op_toALU1 := 0.U
-			io.A_toALU2 := io.A
-	    	io.B_toALU2 := io.B
-	    	io.alu_op_toALU2 := io.alu_op
+		when(!alu2_busy) {
+			io.A_toALU1 		:= 0.U
+	    	io.B_toALU1 		:= 0.U
+	    	io.alu_op_toALU1 	:= 0.U
+			io.A_toALU2 		:= io.A
+	    	io.B_toALU2 		:= io.B
+	    	io.alu_op_toALU2 	:= io.alu_op
 
-	    	alu2_busy := true.B
-		}.otherwise{
-			io.A_toALU1 := 0.U
-	    	io.B_toALU1 := 0.U
-	    	io.alu_op_toALU1 := 0.U
-			io.A_toALU2 := 0.U
-	    	io.B_toALU2 := 0.U
-	    	io.alu_op_toALU2 := 0.U
+	    	alu2_busy 			:= true.B
+		}.otherwise {
+			io.A_toALU1 		:= 0.U
+	    	io.B_toALU1 		:= 0.U
+	    	io.alu_op_toALU1 	:= 0.U
+			io.A_toALU2 		:= 0.U
+	    	io.B_toALU2 		:= 0.U
+	    	io.alu_op_toALU2 	:= 0.U
 		}
 		when(!alu1_busy || io.from_alu1) {
 	    	state := dispatchToALU1
-	    }.otherwise{
+	    }.otherwise {
 	    	state := waitStatus
 	    }
 	}
     .elsewhen(state === waitStatus) {
-		io.A_toALU1 := 0.U
-    	io.B_toALU1 := 0.U
-    	io.alu_op_toALU1 := 0.U
-		io.A_toALU2 := 0.U
-    	io.B_toALU2 := 0.U
-    	io.alu_op_toALU2 := 0.U
+		io.A_toALU1 		:= 0.U
+    	io.B_toALU1 		:= 0.U
+    	io.alu_op_toALU1 	:= 0.U
+		io.A_toALU2 		:= 0.U
+    	io.B_toALU2 		:= 0.U
+    	io.alu_op_toALU2 	:= 0.U
 		
 		when(!alu1_busy || io.from_alu1) {
 	    	state := dispatchToALU1
@@ -109,13 +122,13 @@ class ALU_Dispatcher(implicit val p: Parameters) extends Module with CoreParams 
 	    	state := waitStatus
 	    }
 	}
-	.otherwise{ //unreachable default case
-		io.A_toALU1 := 0.U
-    	io.B_toALU1 := 0.U
-    	io.alu_op_toALU1 := 0.U
-		io.A_toALU2 := 0.U
-    	io.B_toALU2 := 0.U
-    	io.alu_op_toALU2 := 0.U
+	.otherwise { //unreachable default case
+		io.A_toALU1 		:= 0.U
+    	io.B_toALU1 		:= 0.U
+    	io.alu_op_toALU1 	:= 0.U
+		io.A_toALU2 		:= 0.U
+    	io.B_toALU2 		:= 0.U
+    	io.alu_op_toALU2 	:= 0.U
 		state := waitStatus
   	}
 }
--- Code/RiscvMiniBase/src/test/scala/ALU_DispatcherTests.scala
+++ Code/RiscvMiniBase/src/test/scala/ALU_DispatcherTests.scala
@@ -15,12 +15,13 @@ import State._
 
 class ALU_Dispatcher_SW{
   var state: State = dispatch_alu1;
-  var alu1_busy = false;
-  var alu2_busy = false;
+  var alu1_busy    = false;
+  var alu2_busy    = false;
+  var select       = 0;
 
   def step(A: Int, B: Int, alu_op: Int, from_alu1: Boolean, from_alu2: Boolean): Array[Int]={
     //return a list containing A_toALU1, B_toALU1, alu_op_toALU1, A_toALU2, B_toALU2, alu_op_toALU2
-    val output = new Array[Int](6)
+    val output        = new Array[Int](7)
     val previousState = this.state
 
     this.state match{
@@ -87,6 +88,17 @@ class ALU_Dispatcher_SW{
                               }
       case _ => 
     }
+
+    if((from_alu2 && previousState!=dispatch_alu2) && (previousState==dispatch_alu2 || this.alu1_busy)){
+        this.select = 0
+        output(6)   = 0
+    }else if((from_alu1 && previousState!=dispatch_alu1) && (previousState==dispatch_alu1 || this.alu2_busy)){
+        this.select = 1
+        output(6)   = 1
+    }else{
+      output(6) = this.select
+    }
+
     if(from_alu1 && previousState!=dispatch_alu1){
       this.alu1_busy = false
     }
@@ -108,15 +120,18 @@ class ALU_DispatcherTester(c: => ALU_Dispatcher)(implicit p: freechips.rocketchi
   val rs1              = Seq.fill(size)(rnd.nextInt(pow(2, xlen).intValue)) map toBigInt
   val rs2              = Seq.fill(size)(rnd.nextInt(pow(2, xlen).intValue)) map toBigInt
   val alu_op           = Seq.fill(size)(rnd.nextInt(16))
-  val from_alu1        = Seq.fill(size)(rnd.nextBoolean())
-  val from_alu2        = Seq.fill(size)(rnd.nextBoolean())
+  val from_alu1_init   = Seq.fill(size)(rnd.nextBoolean())
+  val from_alu1        = from_alu1_init.updated(0, false)
+  val from_alu2_init   = Seq.fill(size)(rnd.nextBoolean())
+  val from_alu2        = from_alu2_init.updated(0, false)
   val out              = (rs1 zip rs2 zip alu_op zip from_alu1 zip from_alu2) map { case ((((a, b), c), d), e) => dispatcher_model.step(a, b, c, d, e)}
-  val a_toALU1         = VecInit(out map { case Array(a, b, c, d, e, f) => a.U})
-  val b_toALU1         = VecInit(out map { case Array(a, b, c, d, e, f) => b.U})
-  val alu_op_toALU1    = VecInit(out map { case Array(a, b, c, d, e, f) => c.U})
-  val a_toALU2         = VecInit(out map { case Array(a, b, c, d, e, f) => d.U})
-  val b_toALU2         = VecInit(out map { case Array(a, b, c, d, e, f) => e.U})
-  val alu_op_toALU2    = VecInit(out map { case Array(a, b, c, d, e, f) => f.U})
+  val a_toALU1         = VecInit(out map { case Array(a, b, c, d, e, f, g) => a.U})
+  val b_toALU1         = VecInit(out map { case Array(a, b, c, d, e, f, g) => b.U})
+  val alu_op_toALU1    = VecInit(out map { case Array(a, b, c, d, e, f, g) => c.U})
+  val a_toALU2         = VecInit(out map { case Array(a, b, c, d, e, f, g) => d.U})
+  val b_toALU2         = VecInit(out map { case Array(a, b, c, d, e, f, g) => e.U})
+  val alu_op_toALU2    = VecInit(out map { case Array(a, b, c, d, e, f, g) => f.U})
+  val select           = VecInit(out map { case Array(a, b, c, d, e, f, g) => (g==1).B})
 
   dut.io.alu_op := VecInit(alu_op map (_.U))(cntr)
   dut.io.A := VecInit(rs1 map (_.U))(cntr)
@@ -132,10 +147,11 @@ class ALU_DispatcherTester(c: => ALU_Dispatcher)(implicit p: freechips.rocketchi
   assert(dut.io.A_toALU2 === a_toALU2(cntr))
   assert(dut.io.B_toALU2 === b_toALU2(cntr))
   assert(dut.io.alu_op_toALU2 === alu_op_toALU2(cntr))
-  printf("Counter: %d, OP: %d, A: %d, B: %d, from_alu1: %d, from_alu2: %d, OUT: (hw) %d %d %d %d %d %d ?= (sw) %d %d %d %d %d %d\n",
+  assert(dut.io.select === select(cntr))
+  printf("Counter: %d, OP: %d, A: %d, B: %d, from_alu1: %d, from_alu2: %d, OUT: (hw) %d %d %d %d %d %d %d ?= (sw) %d %d %d %d %d %d %d\n",
          cntr, dut.io.alu_op, dut.io.A, dut.io.B, dut.io.from_alu1, dut.io.from_alu2, dut.io.A_toALU1, dut.io.B_toALU1, 
-         dut.io.alu_op_toALU1, dut.io.A_toALU2, dut.io.B_toALU2, dut.io.alu_op_toALU2, a_toALU1(cntr), b_toALU1(cntr),
-         alu_op_toALU1(cntr), a_toALU2(cntr), b_toALU2(cntr), alu_op_toALU2(cntr)) 
+         dut.io.alu_op_toALU1, dut.io.A_toALU2, dut.io.B_toALU2, dut.io.alu_op_toALU2, dut.io.select, a_toALU1(cntr),
+         b_toALU1(cntr), alu_op_toALU1(cntr), a_toALU2(cntr), b_toALU2(cntr), alu_op_toALU2(cntr), select(cntr)) 
 }
 
 class ALU_DispatcherTests extends org.scalatest.FlatSpec {
